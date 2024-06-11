3b60f000f59006a204ef77097458cbb95966a5e5
SamratAdhikari <samratmetaladhikari@gmail.com>
2024-06-11 13:31:18 +0545

    initial commit

---
+++ simulate.py
@@ -0,0 +1,84 @@
+import pygame
+import time
+import numpy as np
+
+# game constants
+COLOR_BG = (10, 10, 10)
+COLOR_GRID = (40, 40, 40)
+COLOR_DIE_NEXT = (170, 170, 170)
+COLOR_ALIVE_NEXT = (46, 139, 87)
+WINDOW_SIZE = (800, 600)
+CELL_SIZE = (60, 80)
+
+
+def update(screen, cells, size=10, with_progress=False):
+	updated_cells = np.zeros((cells.shape[0], cells.shape[1]))
+
+	for row, col in np.ndindex(cells.shape):
+		alive = np.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col] #sum of neighbours
+		color = COLOR_BG if cells[row, col] == 0 else COLOR_ALIVE_NEXT
+
+		if cells[row, col] == 1: # cell is alive currently
+			if alive < 2 or alive > 3:
+				if with_progress:
+					color = COLOR_DIE_NEXT
+
+			elif 2 <= alive <= 3:
+				updated_cells[row, col] = 1 # cell survives next gen
+				if with_progress:
+					color = COLOR_ALIVE_NEXT
+
+		else:
+			if alive == 3:
+				updated_cells[row, col] = 1
+				if with_progress:
+					color = COLOR_ALIVE_NEXT
+
+		# draw individual cells
+		pygame.draw.rect(screen, color, (col*size, row*size, size-1, size-1))
+
+	return updated_cells
+
+
+def main():
+	pygame.init()
+	screen = pygame.display.set_mode(WINDOW_SIZE)
+
+	cells = np.zeros(CELL_SIZE)
+	screen.fill(COLOR_GRID)
+	update(screen, cells)
+
+	pygame.display.flip()
+	pygame.display.update()
+
+	running = False
+
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				return
+
+			elif event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_SPACE:
+					running = not running
+					update(screen, cells)
+					pygame.display.update()
+
+			if pygame.mouse.get_pressed()[0]:
+				pos = pygame.mouse.get_pos()
+				cells[pos[1] // 10, pos[0] // 10] = 1 #10: size
+				update(screen, cells)
+				pygame.display.update()
+
+		screen.fill(COLOR_GRID)
+
+		if running:
+			cells = update(screen, cells, with_progress=True)
+			pygame.display.update()
+
+		time.sleep(0.001)
+
+
+if __name__ == '__main__':
+	main()
